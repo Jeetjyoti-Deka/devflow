77b675ecb4a8d9ae912a3b2699ab7447ddf9bbc3
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2023-11-23 16:15:02 +0530

    Fix error for deployment before webhook

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -8,7 +8,6 @@ import {
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
-import path from "path";
 import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
@@ -70,9 +69,9 @@ export async function deleteUser(params: DeleteUserParams) {
 
     // get user question ids
 
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
 
     // delete user questions
 
