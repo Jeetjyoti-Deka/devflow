c429580404d318324d5e1b98b5c1d4f2c1c31110
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2023-11-26 13:03:21 +0530

    Final changes before webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,9 +7,6 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  console.log("in the webhook function");
-
-  // TODO: add webhook secret to your .env.local
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -35,7 +32,7 @@ export async function POST(req: Request) {
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
+  // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
@@ -54,35 +51,35 @@ export async function POST(req: Request) {
     });
   }
 
-  // Get the ID and type
-  //   const { id } = evt.data;
   const eventType = evt.type;
 
-  console.log(eventType);
+  console.log({ eventType });
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
-    // create a new user in your database
+    // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
-      name: `${first_name} ${last_name}`,
+      name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
       username: username!,
       email: email_addresses[0].email_address,
       picture: image_url,
     });
 
     return NextResponse.json({ message: "OK", user: mongoUser });
-  } else if (eventType === "user.updated") {
+  }
+
+  if (eventType === "user.updated") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
-    // create a new user in your database
+    // Create a new user in your database
     const mongoUser = await updateUser({
       clerkId: id,
       updateData: {
-        name: `${first_name} ${last_name}`,
+        name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
         username: username!,
         email: email_addresses[0].email_address,
         picture: image_url,
@@ -96,7 +93,6 @@ export async function POST(req: Request) {
   if (eventType === "user.deleted") {
     const { id } = evt.data;
 
-    // create a new user in your database
     const deletedUser = await deleteUser({
       clerkId: id!,
     });
@@ -104,5 +100,5 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  return new Response("", { status: 200 });
+  return new Response("", { status: 201 });
 }
--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,17 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api/webhook(.*)",
-    "/questions/:id",
+    "/api/webhook",
+    "question/:id",
     "/tags",
     "/tags/:id",
     "/profile/:id",
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhook(.*)", "/api/chatgpt"],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
