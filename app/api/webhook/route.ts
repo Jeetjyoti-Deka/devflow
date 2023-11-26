9e3b3721440e7253a5308cf22b5ad803e988d150
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2023-11-26 07:58:55 +0530

    add console log

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,6 +7,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+  console.log("in the webhook function");
 
   // TODO: add webhook secret to your .env.local
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
